4e795c5b9322d208216cee018ecec81e1cbd3952
NozoDev <kevinroj326@gmail.com>
2024-06-13 21:50:54 -0500

    estoy haciendo el login para redirigir al dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="register" element={<RegisterPage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import appFirebase from "../firebase/firebase";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+const auth = getAuth(appFirebase);
 
 const LoginPage = () => {
+  const [login, setLogin] = useState(false);
+  const navigate = useNavigate();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const correo = e.target.email.value;
+    const contraseña = e.target.password.value;
+
+    if (login) {
+      try {
+        await signInWithEmailAndPassword(auth, correo, contraseña);
+        console.log("Succefull login");
+        navigate("/dashboard");
+      } catch (error) {
+        console.error("Error loguout", error);
+      }
+    }
+  };
   return (
     <div>
-      
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <input type="text" id="email" placeholder="Ingresa Email" required />
+        <input
+          type="password"
+          id="password"
+          placeholder="Ingresa Contraseña"
+          required
+        />
+        <button type="submit" onClick={() => setLogin(true)}>
+          Login
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
